3de6d50319e445cc707b530a79cd76dfa27d8c00
Alexandra Stroe <alexandra.maria.stroe@gmail.com>
2022-05-14 17:22:28 +0200

    removed null: false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_14_140700) do
+ActiveRecord::Schema.define(version: 2022_05_14_151909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2022_05_14_140700) do
     t.time "offer_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.bigint "tag_id", null: false
     t.index ["tag_id"], name: "index_offers_on_tag_id"
     t.index ["user_id"], name: "index_offers_on_user_id"
