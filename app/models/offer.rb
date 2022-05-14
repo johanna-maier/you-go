8f587701527c7b70ab887dacb7ae0d1740981524
Alexandra Stroe <alexandra.maria.stroe@gmail.com>
2022-05-14 17:03:11 +0200

    added validations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,12 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :address, presence: true, length: { minimum: 6 }
+  validates :price_per_person, numericality: { only_integer: true, greater_than: 0 }, presence: true
+  validates :capacity, numericality: { only_integer: true, greater_than: 0 }, presence: true
+  validates :offer_date, presence: true
+  validates :offer_time, presence: true
 end
