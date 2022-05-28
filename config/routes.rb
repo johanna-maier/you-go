61b7a8d98ce8590280dcf0d5e459c880fe212ce8
Anjali Kumar <anjali.kumar06@gmail.com>
2022-05-28 15:17:55 +0200

    Dashboard skeleton WIP

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   mount Blazer::Engine, at: 'blazer'
   root to: 'pages#home'
+  # get 'dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :offers, only: %i[index show] do
     resources :reviews, only: [:create]
+  end
+
+  resources :dashboard, only: [:index] do
+    resources :user, only: [:update] # edit
   end
 end
