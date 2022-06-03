4ae81a53bbf92f2e306698960aa599a208acc3cf
Alexandra Stroe <alexandra.maria.stroe@gmail.com>
2022-06-03 12:15:55 +0200

    added video to homepage and typing style

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,4 @@
-// Load all the controllers within this directory and all subdirectories. 
+// Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
 import { Application } from "stimulus"
@@ -7,3 +7,43 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+const words = ["RUNNING ]", " CYCLING ]", "SURFING ]", "SKYING ]", "DANCING ]", "PLAYING ]", "CLIMBING ]"] ;
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function() {
+        if (word.length > 0) {
+            document.getElementById('word').innerHTML += word.shift();
+        } else {
+            deletingEffect();
+            return false;
+        };
+        timer = setTimeout(loopTyping, 110);
+    };
+    loopTyping();
+};
+
+function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function() {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('word').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            };
+            typingEffect();
+            return false;
+        };
+        timer = setTimeout(loopDeleting, 110);
+    };
+    loopDeleting();
+};
+
+typingEffect();
