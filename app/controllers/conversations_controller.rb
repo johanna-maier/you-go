d91fab143cc20d28f3347d62a2aaf295c9e53ed4
ieva jirgensone <ieva.ji.mail@gmail.com>
2022-06-04 19:20:10 +0200

    Basic Message form integrated with Offer in Offer#Show

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,6 @@
 class ConversationsController < ApplicationController
   before_action :set_conversation, except: [:index]
-  before_action :check_participating!, except: [:index]
+  # before_action :check_participating!, except: [:index]
 
   def index
     @conversations = policy_scope(Conversation).participating(current_user).order('updated_at DESC')
@@ -12,8 +12,7 @@ class ConversationsController < ApplicationController
   end
 
   def new
-    redirect_to "/dashboard?conversation_id=#{@conversation.id}&page=conversations" and return if @conversation
-
+    # redirect_to "/dashboard?conversation_id=#{@conversation.id}&page=conversations" and return if @conversation
     @message = current_user.messages.build
   end
 
@@ -22,16 +21,16 @@ class ConversationsController < ApplicationController
   def set_conversation
     if params[:receiver_id]
       @receiver = User.find(params[:receiver_id])
-      redirect_to "/dashboard?page=conversations" and return unless @receiver
+      # redirect_to "/dashboard?page=conversations" and return unless @receiver
 
       @conversation = Conversation.between(current_user.id, @receiver.id)[0]
     else
       @conversation = Conversation.find(params[:id])
-      redirect_to "/dashboard?page=conversations" and return unless @conversation&.participates?(current_user)
+      # redirect_to "/dashboard?page=conversations" and return unless @conversation&.participates?(current_user)
     end
   end
 
-  def check_participating!
-    redirect_to "/dashboard?page=conversations" unless @conversation&.participates?(current_user)
-  end
+  # def check_participating!
+  #   redirect_to "/dashboard?page=conversations" unless @conversation&.participates?(current_user)
+  # end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,45 +1,37 @@
 class MessagesController < ApplicationController
-  before_action :set_receiver
   before_action :find_conversation!
-  skip_before_action :find_conversation!, only: :new
 
   def new
     @message = current_user.messages.build
     authorize @message
   end
 
-  # For Testing the offer is simply set to 562. That last bit will need to be removed afterwards!!!
-  # Local ENV Johanna - offer_id 6
   def create
-    @conversation ||= Conversation.new(author_id: current_user.id, receiver_id: @receiver.id, offer_id: 2)
-    @conversation.save! unless @conversation.save
+    @conversation ||= Conversation.create(author_id: current_user.id, receiver_id: receiver_id, offer_id: offer_id)
     @message = current_user.messages.build(message_params)
     @message.conversation_id = @conversation.id
     @message.save!
     authorize @message
 
     flash[:success] = "Your message was sent!"
-    # Might need to redirect somewhere else or not redirect anywhere
     redirect_to "/dashboard?conversation_id=#{@conversation.id}&page=conversations"
   end
 
   private
+
+  def receiver_id
+    params[:receiver_id]
+  end
+
+  def offer_id
+    params[:offer_id]
+  end
 
   def message_params
     params.require(:message).permit(:content)
   end
 
   def find_conversation!
-    @conversation = Conversation.find(params[:conversation_id]) unless params[:conversation_id].nil?
-    # redirect_to conversations_path and return unless @conversation&.participates?(current_user)
-  end
-
-  def set_receiver
-    # For Testing just set to one user: DANIEL
-    # Local ENV Johanna - user_id 2
-    @receiver = User.find(3)
-    # After connecting to Offers, the code below will replace the preset above and will need to be TESTED!!!
-    # @offer = Offer.find(params[:id])
-    # @receiver = @offer.user
+    @conversation = Conversation.find(params[:conversation_id]) unless params[:conversation_id].empty?
   end
 end
