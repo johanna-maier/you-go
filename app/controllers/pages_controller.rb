062cc14040eef2d8b29608e63cf411c7b683e46b
Johanna Maier <joh.maier1@googlemail.com>
2022-06-08 12:26:42 +0200

    Added real recommendation API: If user 66 is logged in, personal recommendations are shown. All other users get recommendations from user 70.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,31 +7,36 @@ class PagesController < ApplicationController
 
     @categories_with_offers = Tag.where("name = category").select { |x| x.offers.count > 1 }
 
+    # Make sure that a user is logged in before trying to access the API
     if current_user.present?
-      recommender_url = "https://recommender-test-4ypjk67m2a-ew.a.run.app/recommender?user_id=#{current_user.id}"
-      # Hard-coded results for Heroku user ID 59 by data team > Only ID that works for API for now.
-      # No matter the user_id, always same result.
+      # If Maria is logged in, she gets her results. For all other loggedin users, Daniels results are displayed.
+      if current_user.id == 66
+        recommender_url = "https://recommender-test-4ypjk67m2a-ew.a.run.app/recommender?user_id=66"
+      else
+        recommender_url = "https://recommender-test-4ypjk67m2a-ew.a.run.app/recommender?user_id=70"
+      end
+
       begin
         response = URI.open(recommender_url).read
         data = JSON.parse(response)
 
         @recommended_offers = []
-        offer_id = Offer.first.id
-
-        data.first(4).each_with_index do |recommendation, index|
-          # recommended_offer = Offer.find(recommendation)
-          # Temporary till we can work with real user IDs + offers that are in DB
-          offer_id += index
-          recommended_offer = Offer.find(offer_id)
-          @recommended_offers << recommended_offer
+        # get first 3 API entries
+        data.first(3).each_with_index do |recommendation_id, index|
+          # Check if offer IDs actually exist in DB
+          if Offer.where(id: recommendation_id).present?
+            recommended_offer = Offer.find(recommendation_id)
+            @recommended_offers << recommended_offer
+          else
+            # If offerr ID is not present, add first (second, third, ...) offer as recommendation.
+            first_offer_id = Offer.first.id
+            @recommended_offers << Offer.find(first_offer_id + index)
+          end
         end
       rescue OpenURI::HTTPError => e
-        puts "Error message: #{e.message}"
-        # Better way to recue API failures?
-        @recommended_offers = Offer.first(4)
+        # If API fails, we just give back the first 3 offers in DB as recommendations.
+        @recommended_offers = Offer.first(3)
       end
-
-
     end
 
   end
